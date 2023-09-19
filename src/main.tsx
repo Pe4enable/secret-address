60c9d0dc12debdc1ddfe56f3ebfa564da3c1e1c3
amdonatusprince <donatusprince@gmail.com>
2023-09-19 09:28:09 +0100

    feat: verxio AI bot

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
+import { WagmiConfig, createConfig, configureChains, } from 'wagmi'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import PageRoutes from './pages/routes.jsx'
+import { gnosis, gnosisChiado } from 'wagmi/chains'
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  RainbowKitProvider,
+  lightTheme
+} from '@rainbow-me/rainbowkit';
+
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [gnosis, gnosisChiado],
+  [
+    publicProvider()
+  ]
+);
+
+const config = createConfig({
+  publicClient,
+  webSocketPublicClient,
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <WagmiConfig config={config}>
+    <RainbowKitProvider 
+      theme={lightTheme({
+        accentColor: '#1570ef',
+        accentColorForeground: 'white',
+        borderRadius: 'small',
+        fontStack: 'system',
+        overlayBlur: 'small'
+      },
+      )} chains={chains}>
+      <PageRoutes />
+      </RainbowKitProvider>
+    </WagmiConfig>
+    
+    </BrowserRouter>
   </React.StrictMode>,
 )
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -15,7 +15,7 @@ import { Withdraw } from '../components/withdraw';
 import logo from '../../src/assets/logo.png'
 import SendReceive from '../svg/send-receive.svg';
 import Shield from '../svg/shield.svg';
-
+import { Link } from 'react-router-dom';
 import { registryAddress, explorer } from '../utils/constants';
 
 import './main.css';
@@ -41,6 +41,7 @@ export function Main() {
               onClick={() => (document.location.href = '/')}
             />
           </div>
+          <Link to="/verxio">
             <button 
             className="hbutton hbutton-lnk header-item" 
             style={{ marginLeft: '-120px' }}>
@@ -49,6 +50,7 @@ export function Main() {
               <BiBot />  
                 </span>
               </button>
+              </Link>
           <div className="header-item ">
             <Connect />
           </div>
@@ -185,3 +187,4 @@ export function Main() {
     </section>
   );
 }
+export default Main;
